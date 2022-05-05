f14d45527b9df7523ea407a0311004a0cdfe0a94
saragenuardi <saretta.summer16@gmail.com>
2022-05-05 17:34:02 +0200

    add bonus

--- js/script.js
+++ js/script.js
@@ -2,11 +2,16 @@ Vue.config.devtools = true;
 
 //Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 
+
+//BONUS
+//Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
 const saluto = new Vue(
     {
         el: '#root',
         data: {
             salutaUtente: "Ciao Sara",
+            immagine: "https://www.unycastile.ch/wp-content/uploads/2020/07/shutterstock_679581043-1024x1024.jpg"
         }
     }
 ); 
